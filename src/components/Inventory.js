f9d295a32fc251589182c2fbc6f60f481a2e01ba
Raminta Alisauskaite <raminta.alisauskaite@gmail.com>
2023-12-01 16:48:06 +0200

    responsive

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React from 'react'
 
-const Inventory = ({info, inventory,slots, sell}) => {
-    return (
-        <div className="f1 inventory">
-            <div>
-                <div className="d-flex justify-content-end h-25">
-                    <b className="mx-2">slots available: {slots}</b>
-                    <img src={info[2].photo} alt=""/>
-                </div>
-                <div className="d-flex flex-wrap">
-                    {inventory.map((goldFound, index) =>
-                        <div key={index} className="box diggedGold d-flex flex-column align-items-center">
-                            <b>{goldFound}g</b>
-                            <img src={info[0].photo} alt=""/>
-                        </div>
-                    )}
-                </div>
-            </div>
+const Inventory = ({ info, inventory, slots, sell }) => {
+	return (
+		<div className='f1 inventory'>
+			<div>
+				<div className='d-flex justify-content-end h-25'>
+					<b className='mx-2'>slots available: {slots}</b>
+					<img src={info[2].photo} alt='' />
+				</div>
+				<div className='inventoryItems d-flex flex-wrap'>
+					{inventory.map((goldFound, index) => (
+						<div
+							key={index}
+							className='box diggedGold d-flex flex-column align-items-center'
+						>
+							<b>{goldFound}g</b>
+							<img src={info[0].photo} alt='' />
+						</div>
+					))}
+				</div>
+			</div>
 
-            <button onClick={sell} className="box sellBtn">SELL ALL GOLD</button>
-        </div>
-    );
-};
+			<button onClick={sell} className='box sellBtn'>
+				SELL ALL GOLD
+			</button>
+		</div>
+	)
+}
 
-export default Inventory;
+export default Inventory
