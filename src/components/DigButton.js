377fcefa8ca24c24a19bfc51a626237741f1975c
Raminta Alisauskaite <raminta.alisauskaite@gmail.com>
2023-12-04 16:35:38 +0200

    mobile version fit to screen bugs fixed

--- src/components/DigButton.js
+++ src/components/DigButton.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-const DigButton = ({addInventory}) => {
-    return (
-        <button onClick={addInventory} className="box digGoldBtn">DIG GOLD</button>
-    );
-};
+const DigButton = ({ addInventory }) => {
+	return (
+		<div style={{ padding: '10px' }}>
+			<button onClick={addInventory} className='box digGoldBtn'>
+				DIG GOLD
+			</button>
+		</div>
+	)
+}
 
-export default DigButton;
+export default DigButton
